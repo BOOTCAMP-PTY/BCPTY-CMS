91ea302410fb1d861310aec03e5a379e140f29e6
JMJHOX <josemiguelaparicio507@gmail.com>
2022-07-21 23:07:31 -0500

    adding bases for cms

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DB_HOST'),
+      port: env.int('DB_PORT'),
+      database: env('DB_NAME'),
+      user: env('DB_USERNAME'),
+      password: env('DB_PASSWORD'),
+    },
+    debug: false,
+  },
+});
