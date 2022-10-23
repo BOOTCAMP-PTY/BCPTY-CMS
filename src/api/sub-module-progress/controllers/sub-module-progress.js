69100e6b8631372ae6a9be18d5169bad826882fb
JMJHOX <josemiguelaparicio507@gmail.com>
2022-10-22 23:37:15 -0500

    added second part new module tracking system

---
+++ src/api/sub-module-progress/controllers/sub-module-progress.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sub-module-progress controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sub-module-progress.sub-module-progress');
